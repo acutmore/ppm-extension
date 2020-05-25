6e0523a09f1a9f80182b72f3298c65b019dc4feb
Ashley Claymore <acutmore@users.noreply.github.com>
2020-05-25 21:47:31 +0100

    Remove existing helloWorld code

--- src/extension.ts
+++ src/extension.ts
@@ -2,35 +2,24 @@ import * as vscode from "vscode";
 import { PPMTreeDataProvider } from "./file-tree";
 import { PPMDecorator } from "./highlight";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+/**
+ * Called by VSCode when the extension is first activated
+ */
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    console.log('PPM activated');
 
-    // This line of code will only be executed once when your extension is activated
-    console.log(
-        'Congratulations, your extension "helloworld-sample" is now active!',
-    );
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand(
-        "extension.helloWorld",
+    context.subscriptions.push(
+    vscode.commands.registerCommand(
+        "extension.ppm",
         () => {
-            // The code you place here will be executed every time your command is executed
-
-            // Display a message box to the user
-            vscode.window.showInformationMessage("Hello World!");
+            vscode.window.setStatusBarMessage("Starting PPM...", 4_000);
             vscode.window.createTreeView("ppm-explorer", {
                 treeDataProvider: new PPMTreeDataProvider(
                     vscode.workspace.rootPath,
                 ),
             });
         },
-    );
-
-    context.subscriptions.push(disposable);
+    ));
 
     const decorator = new PPMDecorator();
     decorator.updateDecorations(vscode.window.activeTextEditor);
