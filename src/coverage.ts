1161ac5ad5a0b55d565c5d1091fda0ed44d842ec
Ashley Claymore <acutmore@users.noreply.github.com>
2020-05-26 07:10:39 +0100

    coverage can be collected in a loop

--- src/coverage.ts
+++ src/coverage.ts
@@ -1,26 +1,67 @@
 import express = require("express");
 import chromeLauncher = require("chrome-launcher");
 import chromeRemote = require("chrome-remote-interface");
-import util = require("util");
 
-// Mostly taken from https://gist.github.com/paulirish/78f46a302083dd757288b5fcc660d75c
+interface Profiler {
+    takePreciseCoverage(): Promise<CoverageReport>;
+}
+
+export interface CoverageReport {
+    timestamp: number;
+    result: {
+        scriptId: string;
+        url: string;
+        functions: {
+            functionMame: string;
+            ranges: {
+                startOffset: number;
+                endOffset: number;
+                count: number;
+            }[];
+            isBlockCoverage: boolean;
+        }[];
+    }[];
+}
 
 export class PPMCoverageCollecter {
+    private readonly cleanups: (() => unknown)[] = [];
+    private profiler: Profiler | undefined = undefined;
+
     constructor(private workspace: string, private httpPort = 8000) {}
 
+    async destroy() {
+        while (this.cleanups.length > 0) {
+            const fn = this.cleanups.pop();
+            await fn!();
+        }
+    }
+
     async start() {
-        const app = express();
-        app.use(express.static(this.workspace));
-        const server = app.listen(this.httpPort, () =>
-            console.log(`Example app listening at http://localhost:${this.httpPort}`),
-        );
+        if (this.profiler) {
+            return;
+        }
 
-        const chrome = await chromeLauncher.launch({
-            handleSIGINT: true,
-        });
-
-        const protocol = await chromeRemote({ port: chrome.port });
+        let success = false;
         try {
+            // Server
+            const app = express();
+            app.use(express.static(this.workspace));
+            const server = app.listen(this.httpPort, () =>
+                console.log(
+                    `Example app listening at http://localhost:${this.httpPort}`,
+                ),
+            );
+            this.cleanups.push(() => server.close());
+
+            // Browser
+            const chrome = await chromeLauncher.launch({
+                handleSIGINT: true,
+            });
+            this.cleanups.push(() => chrome.kill());
+
+            // Remote protocol
+            const protocol = await chromeRemote({ port: chrome.port });
+            this.cleanups.push(() => protocol.close());
             const { Page, Profiler } = protocol;
             await Profiler.enable();
             await Page.enable();
@@ -29,22 +70,30 @@ export class PPMCoverageCollecter {
                 callCount: true,
                 detailed: true,
             });
+            this.cleanups.push(() => Profiler.stopPreciseCoverage());
 
+            // Load Webpage
             Page.navigate({
                 url: `http://localhost:${this.httpPort}/test.html`,
             });
             await Page.loadEventFired();
 
-            const res = await Profiler.takePreciseCoverage();
-            await Profiler.stopPreciseCoverage();
+            this.profiler = Profiler;
+            this.cleanups.push(() => (this.profiler = undefined));
 
-            console.log(util.inspect(res, true, 100));
-        } catch (err) {
-            console.error(err);
+            success = true;
         } finally {
-            protocol.close();
-            chrome.kill();
-            server.close();
+            if (!success) {
+                await this.destroy();
+            }
         }
+    }
+
+    async collectCoverage(): Promise<CoverageReport> {
+        if (!this.profiler) {
+            throw new Error(`coverage collector has not been started`);
+        }
+
+        return await this.profiler.takePreciseCoverage();
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,14 @@
 import * as vscode from "vscode";
 import { PPMTreeDataProvider } from "./file-tree";
 import { PPMDecorator } from "./highlight";
-import {PPMCoverageCollecter} from './coverage';
+import { PPMCoverageCollecter } from "./coverage";
+import { inspect } from "util";
 
 /**
  * Called by VSCode when the extension is first activated
  */
 export function activate(context: vscode.ExtensionContext) {
-    console.log('PPM activated');
+    console.log("PPM activated");
 
     const root = vscode.workspace.rootPath;
     if (!root) {
@@ -16,17 +17,13 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     context.subscriptions.push(
-    vscode.commands.registerCommand(
-        "extension.ppm",
-        () => {
+        vscode.commands.registerCommand("extension.ppm", () => {
             vscode.window.setStatusBarMessage("Starting PPM...", 4_000);
             vscode.window.createTreeView("ppm-explorer", {
-                treeDataProvider: new PPMTreeDataProvider(
-                    root,
-                ),
+                treeDataProvider: new PPMTreeDataProvider(root),
             });
-        },
-    ));
+        }),
+    );
 
     const decorator = new PPMDecorator();
     decorator.updateDecorations(vscode.window.activeTextEditor);
@@ -37,8 +34,25 @@ export function activate(context: vscode.ExtensionContext) {
     );
 
     const converage = new PPMCoverageCollecter(root);
-    converage.start();
+    context.subscriptions.push(
+        new vscode.Disposable(() => converage.destroy()),
+    );
+
+    async function startCollecting() {
+        try {
+            await converage.start();
+            for (let i = 0; i < 100; i++) {
+                const report = await converage.collectCoverage();
+                console.log(inspect(report, false, 100, true));
+                await new Promise((resolve) => setTimeout(resolve, 5_000));
+            }
+        } finally {
+            await converage.destroy();
+        }
+    }
+
+    startCollecting();
 }
 
-// this method is called when your extension is deactivated
+// Called by VSCode when the extension is deactivted
 export function deactivate() {}
